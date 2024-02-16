7b20a9c02a63837cd7260ec5090527eca761dff3
smf264 <smf264@cornell.edu>
2024-02-16 14:47:56 -0500

    working on sec04

--- avg-test.c
+++ avg-test.c
@@ -5,13 +5,13 @@
 
 int avg( int x, int y )
 {
-  int sum = x + x;
-  return sum / 2;
+int sum = x + x;
+return sum / 2;
 }
 
 void test_case_1_basic()
 {
-  printf("\n%s\n", __func__  );
+  printf( "\n%s\n", __func__ );
   ECE2400_CHECK_INT_EQ( avg( 10, 20 ), 15 );
 }
 
@@ -19,10 +19,9 @@ int main( int argc, char* argv[] )
 {
   __n = ( argc == 1 ) ? 0 : atoi( argv[1] );
 
-  if ( (__n <= 0) || (__n == 1) )
+  if ( ( __n <= 0 ) || ( __n == 1 ) )
     test_case_1_basic();
 
   printf( "\n" );
   return __failed;
 }
-
